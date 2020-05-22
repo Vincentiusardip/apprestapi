82287d1d6a2a2f61bb0cc0d3d70f5ffd8fa5910e
vincent <vincentardip@gmail.com>
2020-05-22 11:52:12 +0700

    menambahkan controller

--- controller.js
+++ controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var response = require('./res');
-var connection = require('.koneksi');
+var connection = require('./koneksi');
 
 exports.Index = function(require,res){
-    response.ok("Aplikasi REST API ku berjalan! huwaoww")
+    response.ok("Aplikasi REST API ku berjalan!",res)
 };
--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express ();
 //parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.listen(3000, () => {
-    console.log(`Server started on port`);
+
+//panggil routes
+var routes = require('./routes');
+routes(app);
+
+app.listen(4000, () => {
+    console.log('Server started on port');
 });
