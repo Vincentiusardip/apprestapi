1eed95ed9898b23321793244435db64fb45d089f
vincent <vincentardip@gmail.com>
2020-05-22 12:02:48 +0700

    menambahkan server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express ();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-//panggil routes
+//panggil routes/json
 var routes = require('./routes');
 routes(app);
 
